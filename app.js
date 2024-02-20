4a8631f540048255c9d312eed69b2623c183e5c8
Divyesh2910 <divyeshsinghrathore89@gmail.com>
2024-02-20 20:35:30 +0530

    added routes and controller

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ require("dotenv").config({path: "./.env"});
 
 const express = require("express");
 const app = express();
+// logger
+const logger = require("morgan");
+app.use(logger("tiny"));
+
+// routes
+app.use("/", require("./routes/indexRoutes"));
 
 
-
-app.listen(process.env.PORT, console.log(`server running on port ${process.env.PORT}`));
+app.listen(
+    process.env.PORT, console.log(`server running on port ${process.env.PORT}`));
