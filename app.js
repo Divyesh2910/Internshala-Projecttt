7534532c2e0959bbc16b640eb73f16dd23ebdb42
Divyesh2910 <divyeshsinghrathore89@gmail.com>
2024-02-20 23:13:07 +0530

    added utils and errorhandlers

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use(logger("tiny"));
 // routes
 app.use("/", require("./routes/indexRoutes"));
 
+// error handling
+const errorHandler = require("./utils/errorHandler");
+const { generatedErrors } = require("./middlewares/errors");
+
+app.all("*", (req, res, next) => {
+    next(new errorHandler(`Requested URL Not Found ${req.url}`, 404))
+});
+app.use(generatedErrors);
+
 
 app.listen(
     process.env.PORT, console.log(`server running on port ${process.env.PORT}`));
